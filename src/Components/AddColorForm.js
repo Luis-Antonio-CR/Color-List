a563cc3a3493309d026fc27b33aef376c9249094
Luis-Antonio Castaneda <96863185+Luis-Antonio-CR@users.noreply.github.com>
2022-09-29 19:08:56 +0200

    Changing the addColorForm to a not imperative version

--- src/Components/AddColorForm.js
+++ src/Components/AddColorForm.js
@@ -1,6 +1,6 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 
-const AddColorForm = ({ onNewColor= f => f }) => {
+const AddColorForm1 = ({ onNewColor= f => f }) => {
     const txtTitle = useRef();
     const hexColor = useRef();
 
@@ -17,6 +17,27 @@ const AddColorForm = ({ onNewColor= f => f }) => {
         <form onSubmit={submit}>
             <input ref={txtTitle} type="text" placeholder="color title ..." required />
             <input ref={hexColor} type="color" required />
+            <button>Add</button>
+         </form>
+    )
+}
+
+const AddColorForm = ({ onNewColor= f => f }) => {
+    const [title, setTitle] = useState("");
+    const [color, setColor] = useState("#000");
+
+    const submit = e => {
+        e.preventDefault();
+        onNewColor(title, color);
+        setTitle("");
+        setColor("");
+        
+    }
+
+    return(
+        <form onSubmit={submit}>
+            <input value={title} onChange={event => setTitle(event.target.value)} type="text" placeholder="color title ..." required />
+            <input value={color} onChange={event => setColor(event.target.value)} type="color" required />
             <button>Add</button>
          </form>
     )
